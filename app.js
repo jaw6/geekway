72e95d492bac62664ee118e3441a696c78b408cd
Joshua Wehner <joshua.wehner@gmail.com>
2018-05-15 10:38:16 -0500

    Tweaks to pagination

--- app.js
+++ app.js
@@ -155,7 +155,8 @@ function init(SETTINGS) {
   search.addWidget(
     instantsearch.widgets.pagination({
       container: '#pagination',
-      maxPages: 20,
+      maxPages: 80,
+      padding: 7,
       showFirstLast: false
     })
   );
